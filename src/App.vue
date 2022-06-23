2b6680f5c21ea9808352644f986e93cc43f63c5c
RafaelDP <rafaeld.pena@hotmail.com>
2022-06-23 14:43:15 -0500

    Vue 3 app

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+
+export default{
+data:()=>({
+  contador:10,
+
+}),
+methods:{
+reset(){
+ this.contador=10;
+},
+},
+};
+
+
+
+</script>
+
+<template>
+<div class="container">
+  <img src="./assets/logo.svg" alt="Logo Vue" width="80" height="80" />
+  <h1>Hola Mundo Desde Vue.js 3</h1>
+  <hr />
+  <h2>Contador: {{contador}}</h2>
+  <button @click="contador++">+1</button>
+  <button @click="contador--">-1</button>
+  <button @click="reset()">Reset</button>
+
+</div>
+</template>
+<style scoped>
+
+.container{
+ text-align: center;
+
+}
+h1{
+ font-weight: bolder;
+ color: rgb(73, 72, 72);
+}
+h2{
+  font-size: 2.5rem;
+}
+button{
+  cursor: pointer;
+  font-size: 1.5rem;
+  margin:  0px 5px;
+}
+
+</style>
+
